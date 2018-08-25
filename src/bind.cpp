f91489f1d9829d4e9e8d20b69c70c4acccb6a94f
George <georgi.kasparyants@picsart.com>
2018-08-25 11:56:32 +0400

    bind

--- src/bind.cpp
+++ src/bind.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <functional>
+#include <string>
 
 #include <boost/asio.hpp>
 
 #include <gtest/gtest.h>
 
 
+void caller(std::function<void(void)> const& f) {
+    f();
+}
+
+
+void f1(int n) {
+    std::cout << n << std::endl;
+}
+
+
+void f2(int n, std::string const& s) {
+    std::cout << n << s << std::endl;
+}
+
+
 TEST(BIND, Test1) {
+    auto _f1 = std::bind(f1, 3);
+    auto _f2 = std::bind(f2, 3, "hello");
+    caller(_f1);
+    caller(_f2);
 }
 
 
